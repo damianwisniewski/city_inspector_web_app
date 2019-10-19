198dc65f08d80f076f84da8c5681a2c5fa6ea308
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-10-19 19:02:37 +0200

    Logout modal

--- src/containers/ModalViews/LogoutModal/LogoutModal.jsx
+++ src/containers/ModalViews/LogoutModal/LogoutModal.jsx
@@ -1,12 +1,35 @@
-import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import React from 'react'
+import Button from '../../../components/CommonComponents/Button/Button'
+import { connect } from 'react-redux'
+import { sagaRequestLogout } from '../../../reduxStore/actionCreators/requestActions'
 
-class LogoutModal extends Component {
-	render() {
-		return <div id='LogoutModal'>LogoutModal</div>
+import './LogoutModal.scss'
+
+const LogoutModal = ({ closeModal, onSagaRequestLogout }) => {
+	const handleLogout = () => {
+		onSagaRequestLogout()
+		closeModal()
 	}
+
+	return (
+		<div id='logout-modal' className='logout'>
+			<h2 className='logout__header'>Wyloguj</h2>
+			<p className='logout__context'>Czy jesteś pewien że chcesz się wylogować?</p>
+			<div className='logout__buttons-wrapper'>
+				<Button color='blue' onClick={closeModal}>
+					Nie
+				</Button>
+				<Button color='white' onClick={handleLogout}>
+					Tak
+				</Button>
+			</div>
+		</div>
+	)
 }
 
 // LogoutModal.propTypes = {}
 
-export default LogoutModal
+export default connect(
+	null,
+	{ onSagaRequestLogout: sagaRequestLogout },
+)(LogoutModal)
