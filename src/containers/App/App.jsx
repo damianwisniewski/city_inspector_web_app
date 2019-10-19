99a8a96ffec32fea5e627a3d88f4a90af1351e34
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-10-19 18:53:22 +0200

    check is session is valid yet

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 // Styles
 import './App.scss'
 
+// Containers
 import Navbar from '../Navbar/Navbar'
 
 // Modal Routing
@@ -14,51 +15,68 @@ import { ModalRouter, ModalSwitch, ModalRoute } from '../../components/ModalComp
 import mainRoutes from '../../routes/mainRoutes'
 import modalRoutes from '../../routes/modalRoutes'
 
+// HOC
 import withAuth from '../../HOCs/withAuth/withAuth'
+
+// redux actions
+import { checkSession } from '../../reduxStore/actionCreators/requestActions'
+import RequestStatus from '../../components/CommonComponents/RequestStatus/RequestStatus'
 
 class App extends Component {
 	static defaultProps = {
 		isUserAuth: false,
 	}
 
+	componentDidMount() {
+		this.props.onCheckSession()
+	}
+
 	render() {
 		return (
-			<BrowserRouter>
-				<ModalRouter>
-					<div className='App'>
-						<Navbar />
-						<main className='content'>
-							<Switch>
-								{mainRoutes.map(route => {
-									if (!route.path) {
-										return <Route key={route.path || 'NotFound'} component={route.component} />
-									}
+			<RequestStatus
+				requestState={this.props.isCheckSessionInProgress}
+				fullview
+				color='blue'
+				size='large'
+				direction='vertical'
+			>
+				<BrowserRouter>
+					<ModalRouter>
+						<div className='App'>
+							<Navbar />
+							<main className='content'>
+								<Switch>
+									{mainRoutes.map(route => {
+										if (!route.path) {
+											return <Route key={route.path || 'NotFound'} component={route.component} />
+										}
 
-									return (
-										<Route
-											key={route.path || 'NotFound'}
-											exact={route.exact}
-											path={route.path}
-											component={route.authNeeded ? withAuth(route.component) : route.component}
-										/>
-									)
-								})}
-							</Switch>
-						</main>
-						<ModalSwitch>
-							{modalRoutes.map(route => (
-								<ModalRoute
-									key={route.path || 'NotFound'}
-									darkOverlay={route.darkOverlay}
-									closeButton={route.closeButton}
-									path={route.path}
-									component={route.component}
-								/>
-							))}
-						</ModalSwitch>
-					</div>
-				</ModalRouter>
-			</BrowserRouter>
+										return (
+											<Route
+												key={route.path || 'NotFound'}
+												exact={route.exact}
+												path={route.path}
+												component={route.authNeeded ? withAuth(route.component) : route.component}
+											/>
+										)
+									})}
+								</Switch>
+							</main>
+							<ModalSwitch>
+								{modalRoutes.map(route => (
+									<ModalRoute
+										key={route.path || 'NotFound'}
+										darkOverlay={route.darkOverlay}
+										closeButton={route.closeButton}
+										path={route.path}
+										component={route.component}
+									/>
+								))}
+							</ModalSwitch>
+						</div>
+					</ModalRouter>
+				</BrowserRouter>
+			</RequestStatus>
 		)
 	}
 }
@@ -66,7 +84,15 @@ class App extends Component {
 function mapStateToProps(state) {
 	return {
 		isUserAuth: state.user.isUserAuth,
+		isCheckSessionInProgress: state.requests.checkSessionStatus,
 	}
 }
 
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = {
+	onCheckSession: checkSession,
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)(App)
